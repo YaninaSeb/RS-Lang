9ecac6a9402bb703e8a8bf9eb6e229d4e521f701
KNV1987 <Kalibabchuk@yandex.ru>
2022-02-05 19:38:44 +0300

    fix: удалил ненужные функции

--- src/pages/games/sprint/sprint.ts
+++ src/pages/games/sprint/sprint.ts
@@ -1,5 +1,8 @@
 import { getWords } from '../../../utils/api';
-import { randomFalseWordSprint, renderSprintQuestion } from '../../../utils/listener';
+import {
+  randomFalseWordSprint,
+  renderSprintQuestion,
+} from '../../../utils/listener';
 import { Question } from './qustion';
 import { sprintElement } from './sprint-html';
 import './sprint.scss';
@@ -16,28 +19,21 @@ export class Sprint {
     const sectionMain: HTMLElement | null = document.querySelector('.main');
     const btnBack: HTMLElement | null = document.querySelector('.button__prev');
     const blockQuestion: HTMLElement | null = document.querySelector('.sprint__block-question');
-    
 
-    const arrWords = await getWords(1, 1);
+    const arrWords = await getWords(2, 1);
     btnStart?.addEventListener('click', async () => {
       sections.forEach((section) => section.classList.add('close'));
       sectionQuestion?.classList.remove('close');
       const wordValues = await renderSprintQuestion(3, arrWords);
       const questionInstance = new Question(wordValues.img, wordValues.nameEng, wordValues.nameRus);
       blockQuestion!.innerHTML = await questionInstance.render();
-      await questionInstance.after_render();
-      btnTrue?.addEventListener('click', () => {
-        console.log(randomFalseWordSprint(1, arrWords));
-      })
     });
 
-
-    btnBack?.addEventListener('click', async() => {
+    btnBack?.addEventListener('click', async () => {
       sections.forEach((section) => section.classList.add('close'));
       sectionMain?.classList.remove('close');
     });
 
     const btnTrue: HTMLElement | null = document.querySelector('.btn__true');
-    
   }
 }
--- src/utils/listener.ts
+++ src/utils/listener.ts
@@ -3,15 +3,16 @@ import { wordInterface } from './instance';
 export const renderSprintQuestion = async (id: number, arrWords: wordInterface[]) => {
   const img = arrWords[id].image;
   const nameEng = arrWords[id].word;
-  const randomId = randomFalseWordSprint(id, arrWords)
+  const randomId = randomFalseWordSprint(id, arrWords);
   const nameRus = arrWords[randomId].wordTranslate;
   return { img, nameEng, nameRus };
 };
 
-export const randomFalseWordSprint = (id: number, arrWords: wordInterface[],) => {
+export const randomFalseWordSprint = (id: number, arrWords: wordInterface[]) => {
   let randomNumber = Math.floor(Math.random() * arrWords.length);
   while (randomNumber === id) {
     randomNumber = Math.floor(Math.random() * arrWords.length);
-  }; 
+  }
   return Math.floor(Math.random() * 2) === 1 ? randomNumber : id;
 };
+
