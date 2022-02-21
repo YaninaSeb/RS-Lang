805fd20428ec491d54c66ebbc30a7ce681443734
BlackMamba51 <shvyryov200@gmail.com>
2022-02-21 20:56:02 +0300

    feat: update styles

--- src/components/Header/header-html.ts
+++ src/components/Header/header-html.ts
@@ -1,16 +1,20 @@
 export const headerHTML = ` <header class="nav">
-    <div class="nav-link_to_pages">
-        <a class="link-main" href="./#"><span>Главная</span></a>
-        <a class="link-book" href="./#/book"><span>Учебник</span></a>
-        <a class="link-game_audiocall" href="./#/game-audiocall"><span>Аудиовызов</span></a>
-        <a class="link-game_sprint" href="./#/game-sprint"><span>Спринт</span></a>
-        <a class="link-static" href="./#/statistic"><span>Статистика</span></a>
-    </div>
+    <div class="container">
+        <div class="header__body">
+            <div class="nav-link_to_pages">
+                <a class="link-main" href="./#"><span>Главная</span></a>
+                <a class="link-book" href="./#/book"><span>Учебник</span></a>
+                <a class="link-game_audiocall" href="./#/game-audiocall"><span>Аудиовызов</span></a>
+                <a class="link-game_sprint" href="./#/game-sprint"><span>Спринт</span></a>
+                <a class="link-static" href="./#/statistic"><span>Статистика</span></a>
+            </div>
 
-    <div class="link-authorization">
-        <div class="title-user"></div>
-        <a class="link" href="./#/autorization"><div class="img-login" title="Войти"></div></a>
-        <div class="img-logout" title="Выйти"></div>
+            <div class="link-authorization">
+                <div class="title-user"></div>
+                <a class="link" href="./#/autorization"><div class="img-login" title="Войти"></div></a>
+                <div class="img-logout" title="Выйти"></div>
+            </div>
+        <div>
     </div>
 </header>`;
 
--- src/pages/main/main-html.ts
+++ src/pages/main/main-html.ts
@@ -1,69 +1,76 @@
-export const mainElement = () => `<main>
-<section class="home-greeting">
-    <span class="greeting-title">Откройте мир возможностей вместе с RSLang!</span>
-</section>
+export const mainElement = () => `<main class="main">
 
-<section class="home-about_app">
-    <h1 class="about_app-title">Наши преимущества</h1>
-    
-    <div class="card-privilege">
-        <div class="card-privilege_img" id="img-1"></div>
-        <div class="card-privilege_text">Электронный учебник состоит из шести разделов. Представлены перевод слова, тематическое изображение, а также произношение как слова отдельно, так и в составе словосочетания.</div>
-    </div>
-
-    <div class="card-privilege">
-        <div class="card-privilege_text">Словарь содержит списки изучаемых слов, слов, которые не нужно учить, а также тех, которые вызывают затруднения. В словаре отражена статистика по каждому разделу и успеваемость учащихся.</div>
-        <div class="card-privilege_img" id="img-2"></div>
-    </div>
-
-    <div class="card-privilege">
-        <div class="card-privilege_img" id="img-3"></div>
-        <div class="card-privilege_text">Для изучения слов и закрепления запоминания в приложении есть 2 игры: Спринт и Аудиовызов, которые помогут вам в игровой форме «прокачать» словарный запас.</div>
-    </div>
-
-    <div class="card-privilege">
-        <div class="card-privilege_text">Весь прогресс обучения можно посмотреть в статистике, где представлены данные как за текущий день, так и за весь период обучения. </div>
-        <div class="card-privilege_img" id="img-4"></div>
-    </div>
-</section>
-
-<section class="team">
-    <h1 class="about_team-title">Наша команда</h1>
-
-    <div class="card-team">
-        <div class="card-team_img" id="img-team-1"></div>
-        <div class="card-team_text">
-            <span class="team-person">
-                Коля Калибабчук 
-                <a href="https://github.com/KNV1987"><div class="img-github"></div></a>
-            </span>
-            <span class="team-about_person">Настройка роутера, сделана базовая настройка проекта, создана игра Спринт, создана локальная копия бэкенда, сохранение прогресса игры Спринт</span>
+    <section class="home-greeting">
+        <div class="container">
+            <span class="greeting-title">Откройте мир возможностей вместе с RSLang!</span>
         </div>
-    </div>
+    </section>
 
-    <div class="card-team">
-        <div class="card-team_img" id="img-team-2"></div>
-        <div class="card-team_text">
-            <span class="team-person">
-                Яна Себрукович 
-                <a href="https://github.com/YaninaSeb"><div class="img-github"></div></a>
-            </span>
-            <span class="team-about_person">Сделаны базовые настройки проекта, начальная компоновка, настройка редукса, настройка роутера, форма входа, часть страницы TextBook, игра "Audio Challenge", игра "Savannah", бэкенд</span>
+    <section class="home-about_app">
+        <div class="container">
+            <h1 class="about_app-title">Наши преимущества</h1>
+            
+            <div class="card-privilege">
+                <div class="card-privilege_img" id="img-1"></div>
+                <div class="card-privilege_text">Электронный учебник состоит из шести разделов. Представлены перевод слова, тематическое изображение, а также произношение как слова отдельно, так и в составе словосочетания.</div>
+            </div>
+
+            <div class="card-privilege">
+                <div class="card-privilege_text">Словарь содержит списки изучаемых слов, слов, которые не нужно учить, а также тех, которые вызывают затруднения. В словаре отражена статистика по каждому разделу и успеваемость учащихся.</div>
+                <div class="card-privilege_img" id="img-2"></div>
+            </div>
+
+            <div class="card-privilege">
+                <div class="card-privilege_img" id="img-3"></div>
+                <div class="card-privilege_text">Для изучения слов и закрепления запоминания в приложении есть 2 игры: Спринт и Аудиовызов, которые помогут вам в игровой форме «прокачать» словарный запас.</div>
+            </div>
+
+            <div class="card-privilege">
+                <div class="card-privilege_text">Весь прогресс обучения можно посмотреть в статистике, где представлены данные как за текущий день, так и за весь период обучения. </div>
+                <div class="card-privilege_img" id="img-4"></div>
+            </div>
         </div>
-    </div>
+    </section>
 
-    <div class="card-team">
-        <div class="card-team_img" id="img-team-3"></div>
-        <div class="card-team_text">
-            <span class="team-person">
-                Витя Швырев 
-                <a href="https://github.com/BlackMamba51"><div class="img-github"></div></a>
-            </span>
-            <span class="team-about_person">Создана игра "Аудиовызов", создана страница статистики, сохранение данных статистики на бэкенд</span>
+    <section class="team">
+        <div class="container">
+            <h1 class="about_team-title">Наша команда</h1>
+
+            <div class="card-team">
+                <div class="card-team_img" id="img-team-1"></div>
+                <div class="card-team_text">
+                    <span class="team-person">
+                        Коля Калибабчук 
+                        <a href="https://github.com/KNV1987"><div class="img-github"></div></a>
+                    </span>
+                    <span class="team-about_person">Настройка роутера, сделана базовая настройка проекта, создана игра Спринт, создана локальная копия бэкенда, сохранение прогресса игры Спринт</span>
+                </div>
+            </div>
+
+            <div class="card-team">
+                <div class="card-team_img" id="img-team-2"></div>
+                <div class="card-team_text">
+                    <span class="team-person">
+                        Яна Себрукович 
+                        <a href="https://github.com/YaninaSeb"><div class="img-github"></div></a>
+                    </span>
+                    <span class="team-about_person">Сделаны базовые настройки проекта, начальная компоновка, настройка редукса, настройка роутера, форма входа, часть страницы TextBook, игра "Audio Challenge", игра "Savannah", бэкенд</span>
+                </div>
+            </div>
+
+            <div class="card-team">
+                <div class="card-team_img" id="img-team-3"></div>
+                <div class="card-team_text">
+                    <span class="team-person">
+                        Витя Швырев 
+                        <a href="https://github.com/BlackMamba51"><div class="img-github"></div></a>
+                    </span>
+                    <span class="team-about_person">Создана игра "Аудиовызов", создана страница статистики, сохранение данных статистики на бэкенд</span>
+                </div>
+            </div>
         </div>
-    </div>
 
+    </section>
 
-</section>
 
 </main>`;
